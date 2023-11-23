cff3fdfa14565b96c6e72fcf6e6db3c396b3cb4f
Leakeathon <86569307+Leakeathon@users.noreply.github.com>
2023-11-23 12:02:02 +0000

    Update 2014_10_12_100000_create_password_reset_tokens_table.php

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -10,17 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
 
-        public function down(): void
-    {
-        Schema::dropIfExists('password_reset_tokens');
-    }
+  
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create('password_reset_tokens_jonny', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
+    }
+
+          public function down(): void
+    {
+        Schema::dropIfExists('password_reset_tokens_jonny');
     }
 
     /**
